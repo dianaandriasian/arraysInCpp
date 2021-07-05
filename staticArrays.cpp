49d57350f4e426f3efebb54b82258e51e0e44a24
Diana Andriasyan <86868009+dianaandriasian@users.noreply.github.com>
2021-07-05 15:57:02 +0400

    Update staticArrays.cpp

--- staticArrays.cpp
+++ staticArrays.cpp
@@ -2,7 +2,6 @@
 /// 1-6, 11, 12 ///
 ///////////////////
 
-
 #include <iostream>
 
 void fillArray(int*, int);
@@ -20,24 +19,18 @@ int main()
 {
 	const int s = 10;
 	int arr[s] = {};
-
 	std::cout << "Please, fill array : " << std::endl << std::endl;  
 	fillArray(arr, s);
-
 	std::cout << "Index of array's max element : " << findMaxIndex(arr, s) << std::endl;
 	std::cout << "Index of array's min element : " << findMinIndex(arr, s) << std::endl;
 	std::cout << "MAX + min = " << findMax(arr, s) + findMin(arr, s) << std::endl << std::endl;
-
 	std::cout << "Reverse array : " << std::endl << std::endl; 
 	reverseArray(arr, s);
-
 	int secondArr[s] = {};
 	std::cout << "Please, fill second array : " << std::endl << std::endl; 
 	fillArray(secondArr, s);
-
 	printProduct(arr, secondArr, s);
 	printSum(arr, secondArr, s);
-
 	std::cout << "Count of odds : " << oddNumCount(arr, s) << std::endl;
 	std::cout << "Count of evens : " << evenNumCount(secondArr, s) << std::endl;
 
@@ -50,22 +43,17 @@ void fillArray(int* array, int size)
 		std::cout << "array[" << i << "] = ";
 		std::cin >> array[i];
 	}
-
 	std::cout << std::endl;
 }
 
 int findMax(int* array, int size)
 {
 	int max = array[0];
-
 	for (int i = 1; i < size; ++i) {
-
 		if (max < array[i]) {
-
 			max = array[i];
 		}
 	}
-
 	return max;
 }
 
@@ -73,49 +61,36 @@ int findMaxIndex(int* array, int size)
 {
 	int max = array[0];
 	int index = 0;
-
 	for (int i = 1; i < size; ++i) {
-
 		if (max < array[i]) {
-
 			max = array[i];
 			index = i;
 		}
 	}
-
 	return index;
 }
 
 int findMin(int* array, int size)
 {
 	int min = array[0];
-
 	for (int i = 1; i < size; ++i) {
-
 		if (min > array[i]) {
-
 			min = array[i];
 		}
 	}
-
 	return min;
 }
-
 
 int findMinIndex(int* array, int size)
 {
 	int min = array[0];
 	int index = 0;
-
 	for (int i = 1; i < size; ++i) {
-
 		if (min > array[i]) {
-
 			min = array[i];
 			index = i;
 		}
 	}
-
 	return index;
 }
 
@@ -124,29 +99,24 @@ void reverseArray(int* array, int size)
 	for (int i = size - 1; i >= 0; --i) {
 		std::cout << "array[" << i << "] = " << array[i] << std::endl;
 	}
-
 	std::cout << std::endl;
 }
 
 void printProduct(int* first_array, int* second_array, int size)
 {
 	std::cout << "Array's product : { ";
-
 	for (int i = 0; i < size - 1; ++i) {
 		std::cout << first_array[i] * second_array[i] << ", ";
 	}
-
 	std::cout << first_array[size - 1] * second_array[size - 1] << " } " << std::endl;
 }
 
 void printSum(int* first_array, int* second_array, int size)
 {
 	std::cout << "Array's sum : { ";
-
 	for (int i = 0; i < size - 1; ++i) {
 		std::cout << first_array[i] + second_array[i] << ", ";
 	}
-
 	std::cout << first_array[size - 1] + second_array[size - 1] << " } " << std::endl;
 }
 
@@ -158,7 +128,6 @@ int oddNumCount(int* arr, int s)
 			++count;
 		}
 	}
-
 	return count;
 }
 
@@ -170,6 +139,5 @@ int evenNumCount(int* arr, int s)
 			++count;
 		}
 	}
-
 	return count;
 }
